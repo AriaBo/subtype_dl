c4ae7930f91f431d2448c9804dfc4f1fd4e31417
Arianna Bonetti <arianna.bonetti@tu-dresden.de>
2024-06-12 16:49:11 +0200

    read data

--- sarc_subtypes_dl.py
+++ sarc_subtypes_dl.py
@@ -19,24 +19,7 @@ mpl.rc('ytick', labelsize=12)
 # Specify float format for pandas tables
 pd.options.display.float_format = '{:.3f}'.format
 
-#%%
-#########################
-#READ DATA
-#########################
-df=pd.read_csv("/home/arianna/subtype_dl/data_mrna_seq_v2_rsem.txt", sep='\t')
-df = df.drop(df.columns[1], axis=1)
 
-df_filtered = df.dropna(subset=['Hugo_Symbol'])
-
-# Drop duplicates, keeping only the first occurrence
-df_no_dup = df_filtered.drop_duplicates(subset=['Hugo_Symbol'], keep='first')
-
-df_T=df_no_dup.T
-df_T.iloc[0, 0]="SAMPLE_ID"
-
-print(df_T)
-
-# %%
 #%%
 #####################################
 #GROUNDTRUTH
@@ -61,16 +44,17 @@ print(sarc_truth)
 
 # %%
 #####################################
-#READ MRNA DATA AND PREPROCESS THEM 
+#READ MRNA DATA AND PREPROCESS  
 #####################################
 
-rna=pd.read_csv( "/mnt/bulk/arianna/ATLAS_exp/sarc_tcga_pan_can_atlas_2018/data_mrna_seq_v2_rsem.txt", sep='\t')
+rna=pd.read_csv( "/home/arianna/subtype_dl/data_mrna_seq_v2_rsem.txt", sep='\t')
 
 #traspose + keep only gene symbol
 #-----------------------------
 
 #remove the column with entrez_gene_id
 rna = rna.drop(rna.columns[1], axis=1) 
+
         
 #traspose the rna dataframe 
 rna=rna.T
@@ -84,22 +68,6 @@ rna=rna[1:]  # Return the DataFrame excluding the first row (used for column nam
 
 #remove columns with no gene name 
 rna = rna[rna.columns.dropna()]
-
-
-#remove missing genes
-#-------------------------------
- 
-# Calculate the sum of each column (excluding the first two columns)
-column_sums = rna.sum()
-
-# Filter out columns where the sum is less than 5
-filtered_columns = column_sums[column_sums >= 5].index
-mask = ~column_sums.index.isin(filtered_columns)
-remaining_columns = column_sums.index[mask]
-print(rna[remaining_columns])
-        
-# Keep only the columns where the sum is greater than or equal to 5
-rna = rna[filtered_columns]
 
 print(rna)
 
@@ -117,3 +85,7 @@ sarc_truth_filtered = sarc_truth.loc[common_sample_ids]
 
 # Filter rna to keep only the common sample IDs
 rna_filtered = rna.loc[common_sample_ids]
+
+print(sarc_truth_filtered)
+print(rna_filtered)
+# %%
